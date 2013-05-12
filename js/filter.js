b9aa572be870c72165d6590fc4c57a9a533c95f6
Tamas Hamor <hamuguy@gmail.com>
2013-05-12 11:33:46 -0400

    made it play

--- js/filter.js
+++ js/filter.js
@@ -1,59 +1,67 @@
-$(document).ready(function(){
 
-SC.initialize({
-  client_id: getSCClientId(),
-  redirect_uri: 'https://c9.io/h4mu/scdashfilter/workspace/callback.html'
-});
+SCFilter.prototype.loadSound = function(url, startPlaying) {
+    SC.Widget($('#playerwidget iframe')[0]).load(url, { auto_play : startPlaying });    
+};
 
-SC.get('/me', function(me, error) {
-    if (error) {
-        if (error.message.indexOf('401') === 0) {
-            SC.connect(function() {SC.get('/me', populateUserButton);});
-        } else {
-            console.log(error.message);
-        }
-    } else {
-        populateUserButton(me);
+SCFilter.prototype.isShowable = function(activity) {
+    return activity.origin.streamable && activity.origin.embeddable_by === 'all';
+};
+
+SCFilter.prototype.populateSoundList = function(activities) {
+    if(activities.next_href) {
+        this.next_href = activities.next_href;        
     }
-});
-
-var widgetIframe = document.createElement('iframe');
-$('#playerwidget').empty().append($(widgetIframe).attr(
-    {
-        id:"sc-widget",
-        src:"https://w.soundcloud.com/player/?url=https://api.soundcloud.com/tracks/91595019",
-        width:"100%",
-        height:"170",
-        scrolling:"no",
-        frameborder:"no"
-    }));
-var widget = SC.Widget(widgetIframe);
-
-widget.bind(SC.Widget.Events.READY, function() {
-    widget.getVolume(function(volume) {
-        $('#range').attr({
-          min: 0,
-          max: 100
-        })
-        .val(volume)
-        .change(function() {
-            widget.setVolume($('#range').val());
-            // widget.getVolume(function(volume) {
-            //     console.log('current volume value is ' + volume);
-            // });
+    var activityCollection = activities.collection;
+    var ul = document.createElement('ul');
+    ul.className = 'nav nav-list';
+    for (var i = 0; i < activityCollection.length; i++) {
+        if(this.isShowable(activityCollection[i])) {
+            var origin = activityCollection[i].origin;
+            var li = document.createElement('li');
+            var a = document.createElement('a');
+            a.href = 'javascript:void';
+            li.appendChild(a);
+            a.appendChild(document.createTextNode(origin.user.username + ' / ' + origin.title));
+            $(li).data('uri', origin.uri);
+            ul.appendChild(li);
+        }
+    }
+    $('#sounds').empty().append(ul);
+    SC.oEmbed($(ul.firstChild).data('uri'), function(oembed) {
+        $('#playerwidget').html(oembed.html);
+        var widget = SC.Widget($('#playerwidget iframe')[0]);
+        
+        widget.bind(SC.Widget.Events.READY, function() {
+            widget.getVolume(function(volume) {
+                $('#range').attr({
+                  min: 0,
+                  max: 100
+                })
+                .val(volume)
+                .change(function() {
+                    widget.setVolume($('#range').val());
+                });
+            });
+        });
+        widget.bind(SC.Widget.Events.FINISH, function() {
+            var active = $('li.active');
+            var next = active.next();
+            if(next.length > 0) {
+                active.removeClass('active');
+                widget.load(next.addClass('active').data('uri'), { auto_play : true });
+            }
         });
     });
-});
 
-$('#filter').submit(function() { SC.get('/me/activities', { limit: 50/*, offset: 0*/ }, populateSoundList) });
+    var thisObj = this;
+    $('#sounds ul li').dblclick(function() {
+        thisObj.loadSound($(this).data('uri'), true);
+        $('#sounds ul li').removeClass('active');
+        $(this).addClass('active');
+    });
+};
 
-});
-
-function populateSoundList(activities) {
-    
-}
-
-function populateUserButton(user) {
+SCFilter.prototype.populateUserButton = function(user) {
     $('.authVisible').show();
     var i = document.createElement('i');
     i.className = 'icon-user';
@@ -62,4 +70,25 @@ function populateUserButton(user) {
     .append(i)
     .append(document.createTextNode(user.username))
     .attr('href', user.permalink_url);
+};
+
+SCFilter.prototype.init = function(){
+    SC.initialize({
+      client_id: getSCClientId(),
+      redirect_uri: 'https://c9.io/h4mu/scdashfilter/workspace/callback.html'
+    });
+    
+    var thisObj = this;
+    SC.connect(function() {
+        SC.get('/me', function(user) { thisObj.populateUserButton(user); });
+        SC.get('/me/activities', { limit: 50/*, offset: 0*/ }, function(activities) { thisObj.populateSoundList(activities); });
+    });
+};
+
+function SCFilter() {
+    var next_href = '';
+    var thisObj = this;
+    $(document).ready(function () { thisObj.init(); });
 }
+
+new SCFilter();
