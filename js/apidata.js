bfd1ca29db707a8ab1d92700be267d7f331c71de
Tamas Hamor <hamuguy@gmail.com>
2013-08-08 21:18:11 +0200

    Create apidata.js

---
+++ js/apidata.js
@@ -0,0 +1,9 @@
+var apidata_init = {
+    client_id: 'f8e4b0c1e1c5861ddaa35ec80b7ae5e5',
+    redirect_uri: 'http://h4mu.github.io/SCDashFilter/callback.html'
+};
+
+var apidata_GA = {
+    key: 'UA-41130307-5',
+    domain: 'h4mu.github.io'
+};
